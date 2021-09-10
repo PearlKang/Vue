f9d0521a69e141ff2b4872ce1cf8d6124d225e65
PearlKang <rksk1588@gmail.com>
2021-09-10 21:40:05 +0900

    210910 2
    1

--- 32/vue-todo/src/components/TodoInput.vue
+++ 32/vue-todo/src/components/TodoInput.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>input</div>
+  <!-- <div>input</div> -->
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">추가</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      //1//consolae.log(this.newTodoItem);
+      //2//localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      if(this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
